2ec67eb1708fbbe2fbea60b86ec727c955e25441
Jonathan Tu (laptop) <jhtu@princeton.edu>
2011-02-19 12:02:48 -0500

    Implemented DMD.
    Did a rough implementation of DMD class.  Has not been tested yet.  Requires
    a general method ModalDecomp._compute_inner_product_chunk(), which should be
    moved from bpod.py to modaldecomp.py.  (This has not been done yet.)

--- dmd.py
+++ dmd.py
@@ -1,6 +1,7 @@
 # Parent class
 from modaldecomp import ModalDecomp
 from pod import POD
+import numpy as N
 
 # Common functions
 import util
@@ -16,7 +17,8 @@ class DMD(ModalDecomp):
     
     def __init__(self, load_snap=None, save_mode=None, 
         save_mat=util.write_mat_text, inner_product=None, snapPaths=None,
-        ritzVals=None, ritzVecs=None, ritzVecScaling=None, pod=None):
+        buildCoeff=None, ritzVals=None, lowOrderEigVecs=None, ritzVecScaling=None, 
+        modeEnergies=None, pod=None):
         """
         DMD constructor
         
@@ -31,14 +33,15 @@ class DMD(ModalDecomp):
 
         # Additional data members
         self.snapPaths = snapPaths
+        self.buildCoeff = buildCoeff
         
         # Data members that will be set after computation
         self.ritzVals = ritzVals
-        self.ritzVecs = ritzVecs
+        self.lowOrderEigVecs = lowOrderEigVecs
         self.ritzVecScaling = ritzVecScaling
+        self.modeEnergies = modeEnergies
         self.pod = pod
-
-        # TO DO: DMD objects should contain an internal POD object.
+        
         print 'DMD constructor.'
         
     def compute_decomp(self, ritzEigvalsPath=None, ritzVecsPath=None, 
@@ -50,16 +53,76 @@ class DMD(ModalDecomp):
         ritzVecNormsPath - Output path for matrix of Ritz vectors.
             
         """
-        # Check if a pod object has been loaded yet, then compute as necessary
-        print 'Computing DMD decomposition.'
-    
+        
+        # Compute POD from snapshots (excluding last snapshot)
+        if self.pod is None:
+            self.pod = POD(self.load_snap, self.inner_product, 
+                self.snapPaths[:-1])
+            self.pod.compute_decomp()
+        elif self.snaplist[:-1] != self.podsnaplist or len(snapPaths) !=\
+            len(self.pod.snapPaths)+1 ):
+            raise RuntimeError('Snapshot mistmatch between POD and DMD '+\
+                'objects.')      
+        _podSingValsMat = N.mat(N.diag(N.array(self.pod.singVals).squeeze()))
+                
+        # Inner product of snapshots w/POD modes
+        numSnaps = len(self.SnapPaths)
+        podModesStarTimesSnaps = N.mat(N.empty((numSnaps-1, numSnaps-1)))
+        podModesStarTimesSnaps[:,0:-1] = self.pod.correlationMatrix[:,1:]
+        podModesStarTimesSnaps[:,-1] = self._compute_inner_product_chunk( self.\
+            snapPaths[:-1], self.snapPaths[-1] )
+        podModesStarTimesSnaps = (_podSingValsMat ** -0.5) * self.pod.\
+            singVecs.H * modesStarTimesSnaps
+            
+        # Reduced order linear system
+        self.lowOrderLinearMap = podModesStarTimesSnaps * self.pod.singVecs * \
+            (_podSingValsMat ** -0.5) 
+        self.ritzValues, self.lowOrderEigVecs = N.linalg.eig(lowOrderLinearMap)
+        
+        # Scale Ritz vectors
+        ritzVecsStarTimesInitSnap = self.lowOrderEigVecs.H * (_podSingValsMat \
+            ** -0.5 ) * self.pod.singVecs.H, self.pod.correlationMat[:,0]
+        self.ritzVecScaling = N.linalg.inv(self.lowOrderEigVecs.H, self.\
+            lowOrderEigVecs) * ritzVecsStarTimesInitSnap
+
+        # Compute mode energies
+        self.buildCoeff = self.pod.singVecs * (_podSingValsMat ** -0.5) *\
+            self.lowOrderEigVecs * N.diag(self.d)
+        self.modeEnergies = N.sqrt(N.diag(buildCoeff.H * self.pod.\
+            correlationMatXTX * buildCoeff)).real
+        
     def compute_modes(self, modeNumList, modePath, indexFrom=1 ):
         # Call base class method w/correct coefficients for modes
-        self.ritzVecs = 1
-        self.ritzVecScaling = 1
+        """ Add error checking for definition of various matrices"""
         ModalDecomp.compute_modes(self, modeNumList, modePath, 
-            self.snapPaths,self.ritzVecs*self.ritzVecScaling, 
-            indexFrom=indexFrom )
+            self.buildCoeff, indexFrom=indexFrom )
         print 'Implemented to compute DMD modes.'
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         
         
