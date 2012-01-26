9cd80e9319a122d00e304d66095fe9f20bf3e72f
Brandt Belson (rainier) <bbelson@princeton.edu>
2012-01-25 23:29:23 -0500

    More interface changes to ERA.
    Now less restrictive about the time_steps.
    It is not necessary to give the ERA class any information about the time steps.
    Some functions are now moved outside of the ERA class and are available mostly for
    convenience, they are not essential.
    These are for converting time step arrays and converting equally spaced data
    to sampled data.

--- era.py
+++ era.py
@@ -2,6 +2,60 @@
 import numpy as N
 import util
 
+def sample_times(num_steps, interval):
+    """Returns int array of time steps [0 1 interval interval+1 ...]"""
+    if num_steps%2 != 0:
+        raise ValueError('num_steps must be even, you gave %d'%num_steps)
+    interval = int(interval)
+    time_steps = N.zeros(num_steps, dtype=int)
+    time_steps[::2] = interval*N.arange(num_steps/2)
+    time_steps[1::2] = 1 + interval*N.arange(num_steps/2)
+    return time_steps
+
+
+def make_sampled_format(times, outputs):
+    """
+    Converts equally spaced samples and makes into sampled format [0 1 P P+1 ...].
+    
+    Returns time_steps, outputs, and dt.
+    
+    The times input argument can be time steps (ints) or values (floats).
+    If the input times are already in the sampled format, the returned arrays
+    are equal to the input ones.
+    """
+    num_time_steps, num_outputs, num_inputs = outputs.shape
+    if num_time_steps != times.shape[0]:
+        raise RuntimeError('Size of time and output arrays differ')
+
+    dt_system = times[1] - times[0]
+    dt_sample = times[2] - times[0]
+    time_steps = N.round((times-times[0])/dt_system)
+    
+    if N.abs(2*dt_system - dt_sample) < 1e-8:
+        # Format [0, 1, 2, 3, ...], requires conversion
+        num_time_steps_corr = (num_time_steps - 1)*2
+        outputs_corr = N.zeros((num_time_steps_corr, num_outputs, num_inputs))
+        time_steps_corr = N.zeros(num_time_steps_corr,dtype=int)
+        outputs_corr[::2] = outputs[:-1]
+        outputs_corr[1::2] = outputs[1:]
+        time_steps_corr[::2] = time_steps[:-1]
+        time_steps_corr[1::2] = time_steps[1:]
+        true_time_steps = sample_times(num_time_steps_corr, 1)
+    else:
+        # Format [0, 1, P, P+1, ...]
+        # Make number of time steps even, using integer division
+        num_time_steps_corr = (num_time_steps/2) * 2
+        time_steps_corr = time_steps[:num_time_steps_corr]
+        outputs_corr = outputs[:num_time_steps_corr]
+        true_time_steps = sample_times(num_time_steps_corr, int(round(dt_sample/dt_system)))
+    
+    if (time_steps_corr != true_time_steps).any():
+        raise ValueError('Time values do not make sense')
+    
+    return true_time_steps, outputs_corr, dt_system
+
+
+
 class ERA(object):
     """Forms ERA ROM for discrete-time system from impulse output data 
 
@@ -10,9 +64,9 @@ class ERA(object):
       output_paths =['/path/in1ToOuts.txt', '/path/in2ToOuts.txt']
       myERA = ERA()
       time_values, outputs = util.load_impulse_outputs(output_paths)
-      myERA.set_impulse_outputs(time_values, outputs)
-      myERA.compute_decomp()
-      myERA.save_decomp('H.txt','H2.txt','U.txt','E.txt','V.txt')
+      # Optional for equally spaced time data, requires fewer outputs
+      time_steps, outputs, dt = make_sampled_format(time_values, outputs)
+      myERA.set_outputs(outputs, dt)
       myERA.compute_ROM(50)
       myERA.save_ROM('A.txt','B.txt','C.txt')
       
@@ -32,9 +86,8 @@ class ERA(object):
     
       t0 + dt_system*[0, 1, 2, 3, 4, ... ]
     
-    Currently *only the second format* appears to give balanced models with good accuracy.
-    It's possible that the first format does as well, but we aren't sure.
-    For now, it's recommended to use only the special case format, P=2.
+    Currently for P>=3 we aren't sure if the models are correct.
+    For now, it's recommended to use only P=2 or P=1.
     
     Not intended to be parallelized for distributed memory.
     
@@ -58,150 +111,79 @@ class ERA(object):
         # Used for error checking and determing file formats
         self.dt_tol = 1e-6
      
-    def set_impulse_outputs(self, time_values, outputs):
+    def set_outputs(self, outputs, dt=None):
         """
         Set the signals from each output to each input.
         
-        Input arguments:
         outputs: 
           3D array of Markov parameters with indices [time_interval#, output#, input#],
           so that outputs[snap_num] is the Markov parameter C A**i B at that time interval number.
-          outputs = [CB, CAB, CA**PB, CA**(P+1)B, ...]   OR 
-          output = [CB, CAB, CA**2B, ...]
-        
-        time_values: 
-          corresponding to outputs
-          For sequence 1, this would be dt*[0, 1, P, P+1, 2P, 2P+1, ...].
-          For sequence 2, this would be dt*[0, 1, 2, 3, 4, ...].
-        
-        This function is particularly useful if the data is not saved in the default
-        format (which can be loaded with self.load_impulse_outputs).
-        
-        Sets self.outputs, self.time_steps, and self.dt_system.
+          outputs = [CB, CAB, CA**PB, CA**(P+1)B, ...]
+          
+        Sets self.outputs and self.dt.
         """
-        self.time_steps, self.outputs, self.dt_system = \
-            self._correct_format(time_values, outputs[:time_values.shape[0]]) 
+        if dt is None:
+            self.dt = 1.
+        else:
+            self.dt = dt
+            
+        self.outputs = outputs
+        ndims = self.outputs.ndim
+        if ndims == 1:
+            self.outputs = self.outputs.reshape((self.outputs.shape[0],1,1))
+        elif ndims == 2:
+            self.outputs = self.outputs.reshape((self.outputs.shape[0],self.outputs.shape[1],1))
+        elif ndims > 3: 
+            raise RuntimeError('outputs can have 1, 2, or 3 dims, yours had %d'%ndims)
         
+        self.num_time_steps, self.num_outputs, self.num_inputs = self.outputs.shape
         
-    """ 
-    Moved to util, since it isn't a necessary component of ERA.
-    It's a convenience.
-    Not sure what the right place for it is though.
-    
-    def load_impulse_outputs(self, output_paths):
-        
-        Loads impulse outputs with format [t out1 out2 ...].
-        
-        The output times must be in one of two formats:
-        t0, t0+dt_system, t0+dt_sample, t0+dt_sample+dt_system, t0+2*dt_sample, ...
-        Or, if dt_system and dt_sample are equal:
-        t0, t0+dt_sample, t0+2*dt_sample, ...
-        
-        output_paths is a list of the files, each corresponding to a particular
-        input's impulse response.
-        
-        num_inputs = len(output_paths)
-        
-        # Read the first file to get parameters
-        raw_data = self.load_mat(output_paths[0])
-        num_outputs = raw_data.shape[1]-1
-        if num_outputs == 0:
-            raise ValueError('Data must have at least two columns')
-        time_values = raw_data[:, 0]
-        num_time_values = len(time_values)
-        
-        # Now allocate array and read all of the output data
-        outputs_read = N.zeros((num_time_values, num_outputs, num_inputs))
-        
-        # Load all of the outputs, make sure time_values match for each input impulse file
-        for input_num,output_path in enumerate(output_paths):
-            raw_data = self.load_mat(output_path)
-            time_values_read = raw_data[:,0]
-            if N.amax(N.abs(time_values_read - time_values)) > self.dt_tol:
-                raise ValueError('Time values in %s are inconsistent with other files'%output_path)   
-            outputs_read[:,:,input_num] = raw_data[:,1:]
-                
-        self.time_steps, self.outputs, self.dt_system = self._correct_format(time_values, 
-            outputs_read)
-    """           
+        # Must have an even number of time steps, remove last entry if odd.
+        if self.num_time_steps%2 != 0:
+            self.num_time_steps -= 1
+            self.outputs = self.outputs[:-2]
+            
     
     
-    def compute_decomp(self, mc=None, mo=None):
+    def compute_ROM(self, num_states, dt=None, mc=None, mo=None):
         """
-        Assembles the Hankel matrices from outputs and takes SVD
-        
-        Assumes that the impulse output signals are set as self.outputs
-        L_sing_vecs*N.mat(N.diag(sing_vals))*R_sing_vecs.H = Hankel_mat
-        """
-        if mc is not None: self.mc = mc
-        if mo is not None: self.mo = mo
-        if self.outputs is None:
-            raise util.UndefinedError('No output impulse data exists in instance')
-               
-        self._assemble_Hankel()
-        self.L_sing_vecs, self.sing_vals, self.R_sing_vecs = util.svd(self.Hankel_mat) 
-    
-       
-        
-    def compute_ROM(self, num_states, num_inputs=None, num_outputs=None, dt_system=None):
-        """
-        Computes the A,B,C LTI ROM matrices from the SVD matrices.
+        Computes the A,B,C LTI ROM matrices.
         
         num_states:
           the number of states to be found for the ROM.
-        If using loaded SVD data, then must give the num_inputs, num_outputs, and dt_system.
-        If using impulse response data, this is unnecessary.
+        dt:
+          The time step of the discrete system, *not* the sampling period ("P").
         
-        For discrete time systems the, impulse is applied over a time interval dt_system and so
-        has a time-integral 1*dt_system rather than 1. This means the B matrix is "off" by a
-        factor of dt_system. This is accounted for by multiplying B by dt_system.
+        Assembles the Hankel matrices from outputs and takes SVD
+        L_sing_vecs*N.mat(N.diag(sing_vals))*R_sing_vecs.H = Hankel_mat
+                
+        For discrete time systems the impulse is applied over a time interval dt and so
+        has a time-integral 1*dt rather than 1. This means the B matrix is "off" by a
+        factor of dt. This is accounted for by multiplying B by dt.
         """
-      
-        if self.outputs is not None:
-            num_time_steps, num_outputs, num_inputs = self.outputs.shape
-        elif dt_system is not None:
-            self.dt_system = dt_system
-        elif num_outputs is None or num_inputs is None or self.dt_system is None:
-            raise util.UndefinedError('Specify number of outputs, inputs, and dt_system')
+
+        if self.outputs is None:
+            raise util.UndefinedError('No output impulse data exists in instance')
         
-        # Truncated matrices
+        self.mc = mc
+        self.mo = mo
+
+        self._assemble_Hankel()
+        self.L_sing_vecs, self.sing_vals, self.R_sing_vecs = util.svd(self.Hankel_mat) 
+
+        # Truncate matrices
         Ur = N.mat(self.L_sing_vecs[:,:num_states])
         Er = N.squeeze(self.sing_vals[:num_states])
         Vr = N.mat(self.R_sing_vecs[:,:num_states])
         
         self.A = N.mat(N.diag(Er**-.5)) * Ur.H * self.Hankel_mat2 * Vr * N.mat(N.diag(Er**-.5))
-        self.B = (N.mat(N.diag(Er**.5)) * (Vr.H)[:,:num_inputs]) * self.dt_system 
+        self.B = (N.mat(N.diag(Er**.5)) * (Vr.H)[:,:self.num_inputs]) * self.dt 
         # !! NEED * dt above!!
-        self.C = Ur[:num_outputs,:] * N.mat(N.diag(Er**.5))
+        self.C = Ur[:self.num_outputs,:] * N.mat(N.diag(Er**.5))
         
         if (N.linalg.eig(self.A)[0] >= 1.).any() and self.verbose:
             print 'Warning: Unstable eigenvalues of ROM matrix A'
 
- 
- 
-    def save_decomp(self, Hankel_mat_path, Hankel_mat2_path, L_sing_vecs_path, sing_valsPath,
-        R_sing_vecs_path):
-        """Saves the decomposition matrices"""
-        self.save_mat(self.Hankel_mat, Hankel_mat_path)
-        self.save_mat(self.Hankel_mat2,Hankel_mat2_path)
-        self.save_mat(self.L_sing_vecs,L_sing_vecs_path)
-        self.save_mat(self.sing_vals,sing_valsPath)
-        self.save_mat(self.R_sing_vecs,R_sing_vecs_path)
- 
- 
-        
-    def load_decomp(self, Hankel_mat_path, Hankel_mat2_path, L_sing_vecs_path, sing_valsPath,
-        R_sing_vecs_path):
-        """Loads the decomposition matrices, computes SVD if necessary"""
-        self.L_sing_vecs=self.load_mat(L_sing_vecs_path)
-        self.R_sing_vecs=self.load_mat(R_sing_vecs_path)    
-        self.sing_vals=self.load_mat(sing_valsPath)    
-        self.Hankel_mat=self.load_mat(Hankel_mat_path)
-        self.Hankel_mat2=self.load_mat(Hankel_mat2_path)
-        if self.Hankel_mat.shape != self.Hankel_mat2.shape:
-            raise RuntimeError('Sizes of Hankel and Hankel2 matrices differ')
-                
- 
  
     def save_ROM(self, APath, BPath, CPath):
         """Saves the A, B, and C LTI matrices to file"""  
@@ -212,7 +194,18 @@ class ERA(object):
         print APath
         print BPath
         print CPath
-    
+     
+ 
+    def save_decomp(self, Hankel_mat_path, Hankel_mat2_path, L_sing_vecs_path, sing_valsPath,
+        R_sing_vecs_path):
+        """Saves the decomposition and Hankel matrices"""
+        self.save_mat(self.Hankel_mat, Hankel_mat_path)
+        self.save_mat(self.Hankel_mat2,Hankel_mat2_path)
+        self.save_mat(self.L_sing_vecs,L_sing_vecs_path)
+        self.save_mat(self.sing_vals,sing_valsPath)
+        self.save_mat(self.R_sing_vecs,R_sing_vecs_path)
+ 
+  
  
  
     def _assemble_Hankel(self):
@@ -220,91 +213,33 @@ class ERA(object):
         Assembles and sets self.Hankel_mat and self.Hankel_mat2 (H and H' in Ma 2011).        
         
         Chooses mc and mo to be equal and maximal for number of time steps.
-        Consider (let sample_interval=P=1 for clarity) sequences
+        To understand how it chooses mc and mo, consider (let sample_interval=P=1 w.l.o.g.)
+        sequences
         [0 1 1 2 2 3] => H = [[0 1][1 2]]   H2 = [[1 2][2 3]], mc=mo=1, nt=6
         [0 1 1 2 2 3 3 4 4 5] => H=[[0 1 2][1 2 3][2 3 4]] H2=[[1 2 3][2 3 4][3 4 5]], mc=mo=2,nt=10
         Thus, using all available data means mc=mo=(nt-2)/4.
         For additional output times, (nt-2)/4 rounds down, so we use this formula.
         """
-        num_time_steps, num_outputs, num_inputs = N.shape(self.outputs)
-        assert(num_time_steps%2==0)
         
         if self.mo is None or self.mc is None:
             # Set mo and mc, time_steps is always in format [0, 1, P, P+1, ...]
-            self.mo = (num_time_steps-2)/4
+            self.mo = (self.num_time_steps-2)/4
             self.mc = self.mo
                 
-        if (self.mo + self.mc) +2 > num_time_steps:
+        if (self.mo + self.mc) +2 > self.num_time_steps:
             raise ValueError('mo+mc+2=%d and must be <= than the number of samples %d'%(
                 self.mo+self.mc+2, num_time_steps))
         
-        self.Hankel_mat = N.zeros((num_outputs*self.mo, num_inputs*self.mc))
+        self.Hankel_mat = N.zeros((self.num_outputs*self.mo, self.num_inputs*self.mc))
         self.Hankel_mat2 = N.zeros(self.Hankel_mat.shape)
         #outputs_flattened = self.outputs.swapaxes(0,1).reshape((num_outputs, -1))
         for row in range(self.mo):
-            row_start = row*num_outputs
-            row_end = row_start + num_outputs
+            row_start = row*self.num_outputs
+            row_end = row_start + self.num_outputs
             for col in range(self.mc):
-                col_start = col*num_inputs
-                col_end = col_start + num_inputs
+                col_start = col*self.num_inputs
+                col_end = col_start + self.num_inputs
                 self.Hankel_mat[row_start:row_end, col_start:col_end] = self.outputs[2*(row+col)]
                 self.Hankel_mat2[row_start:row_end, col_start:col_end] = self.outputs[2*(row+col)+1]
                 
-        
-        
-    def _correct_format(self, time_values, outputs):
-        """
-        Returns time_steps and outputs in format [CB, CAB, CA**P, CA**(P+1)B, ...]
-        
-        This converts the case of [CB, CAB, CA**2B, ...] into
-        the general form, ie [CB, CAB, CA**PB, CA**(P+1)B, CA**(2P)B, ...].
-        This means there can be repeated entries. 
-        """
-        if time_values.ndim != 1:
-            raise RuntimeError('time_values must be a 1D array')
-        
-        ndims = outputs.ndim
-        if ndims == 1:
-            print 'Warning: Reshaping outputs from 1D array to 3D'
-            outputs = outputs.reshape((outputs.shape[0],1,1))
-        elif ndims == 2:
-            print 'Warning: Reshaping outputs from 2D array to 3D'
-            outputs = outputs.reshape((outputs.shape[0],outputs.shape[1],1))
-        elif ndims > 3: 
-            raise RuntimeError('outputs can have 1, 2, or 3 dimn, yours had %d'%ndims)
-            
-        num_time_steps, num_outputs, num_inputs = outputs.shape
-        if num_time_steps != time_values.shape[0]:
-            raise RuntimeError('Size of time and output arrays differ')
-
-        dt_system = time_values[1] - time_values[0]
-        dt_sample = time_values[2] - time_values[0]
-        time_steps = N.round((time_values-time_values[0])/dt_system)
-        
-        if N.abs(2*dt_system - dt_sample) < self.dt_tol:
-            # Format [0, 1, 2, 3, ...], requires conversion
-            num_time_steps_corr = (num_time_steps - 1)*2
-            outputs_corr = N.zeros((num_time_steps_corr, num_outputs, num_inputs))
-            time_steps_corr = N.zeros(num_time_steps_corr)
-            outputs_corr[::2] = outputs[:-1]
-            outputs_corr[1::2] = outputs[1:]
-            time_steps_corr[::2] = time_steps[:-1]
-            time_steps_corr[1::2] = time_steps[1:]
-            true_time_steps = N.zeros(num_time_steps_corr)
-            true_time_steps[::2] = N.arange(num_time_steps_corr/2)
-            true_time_steps[1::2] = 1 + N.arange(num_time_steps_corr/2)
-        else:
-            # Format [0, 1, P, P+1, ...]
-            # Make number of time steps even, using integer division
-            num_time_steps_corr = (num_time_steps/2) * 2
-            time_steps_corr = time_steps[:num_time_steps_corr]
-            outputs_corr = outputs[:num_time_steps_corr]
-            true_time_steps = N.zeros(num_time_steps_corr)
-            true_time_steps[::2] = round(dt_sample/dt_system) * N.arange(num_time_steps_corr/2)
-            true_time_steps[1::2] = 1 + round(dt_sample/dt_system) * N.arange(num_time_steps_corr/2)
-        
-        
-        if (time_steps_corr != true_time_steps).any():
-            raise ValueError('Time values do not make sense')
-        
-        return time_steps_corr, outputs_corr, dt_system
+                
--- testera.py
+++ testera.py
@@ -20,7 +20,7 @@ class testERA(unittest.TestCase):
     
     
     #@unittest.skip('Testing others')
-    def test_correct_format(self):
+    def test_make_sampled_format(self):
         """
         Test that can give time_values and outputs in either format.
         
@@ -29,23 +29,21 @@ class testERA(unittest.TestCase):
         """
         for num_inputs in [1,3]:
             for num_outputs in [1, 2,4]:
-                for num_time_steps in [4, 10, 11]:
+                for num_time_steps in [4, 10, 12]:
                     for sample_interval in [1, 2]:
                         # With real data, if P=2 then the data is in the second format.
                         dt_system = N.random.random()
                         dt_sample = sample_interval*dt_system
-                        time_values = N.zeros(num_time_steps)
-                        time_values[::2] = dt_sample * N.arange(int(N.ceil(num_time_steps/2.)))
-                        time_values[1::2] = dt_system + dt_sample * N.arange(num_time_steps/2)
+                        time_steps_true = era.sample_times(num_time_steps, int(dt_sample/dt_system))
+                        time_values = dt_system*time_steps_true
                         outputs = N.random.random((num_time_steps, num_outputs, num_inputs)) 
     
                         my_ERA = era.ERA()
                         time_steps_computed, outputs_computed, dt_system_computed = \
-                            my_ERA._correct_format(time_values, outputs)
+                            era.make_sampled_format(time_values, outputs)
                         if sample_interval != 2:
                             # Format [0 1 P P+1 ...]
                             num_time_steps_true = (num_time_steps/2) *2
-                            time_steps_true = N.round(time_values/dt_system)[:num_time_steps_true]
                             N.testing.assert_allclose(time_steps_computed, time_steps_true)
                             N.testing.assert_allclose(outputs_computed, 
                                 outputs[:num_time_steps_true])
@@ -53,11 +51,9 @@ class testERA(unittest.TestCase):
                         else:
                             # Format [0, 1, 2, 3, ...]
                             num_time_steps_true = (num_time_steps - 1)*2
-                            time_steps_true = N.zeros(num_time_steps_true)
-                            time_steps_true[::2] = N.arange(num_time_steps_true/2)
-                            time_steps_true[1::2] = 1 + N.arange(num_time_steps_true/2)
+                            time_steps_true = era.sample_times(num_time_steps_true, 1)
                             time_steps_computed, outputs_computed, dt_system_computed = \
-                                my_ERA._correct_format(time_values, outputs)
+                                era.make_sampled_format(time_values, outputs)
                             outputs_true = N.zeros((num_time_steps_true, num_outputs, num_inputs))
                             outputs_true[::2] = outputs[:-1]
                             outputs_true[1::2] = outputs[1:]
@@ -67,7 +63,7 @@ class testERA(unittest.TestCase):
 
                         # Test that if there is a wrong time value, get an error
                         time_values[num_time_steps/2] = -1
-                        self.assertRaises(ValueError, my_ERA._correct_format, time_values, outputs)
+                        self.assertRaises(ValueError, era.make_sampled_format, time_values, outputs)
     
                         
     #@unittest.skip("testing others")
@@ -80,12 +76,10 @@ class testERA(unittest.TestCase):
                     num_time_steps = 50
                     num_states = 5
                     A,B,C = util.drss(num_states, num_inputs, num_outputs)
-                    time_steps = N.zeros(num_time_steps,dtype=int)
-                    time_steps[::2] = sample_interval*N.arange(num_time_steps/2)
-                    time_steps[1::2] = 1 + sample_interval*N.arange(num_time_steps/2)
+                    time_steps = era.sample_times(num_time_steps, sample_interval)
                     time_steps, outputs = util.impulse(A, B, C, time_steps=time_steps)
                     myERA = era.ERA()
-                    myERA.set_impulse_outputs(time_steps, outputs)
+                    myERA.set_outputs(outputs)
                     myERA._assemble_Hankel()
                     H = myERA.Hankel_mat
                     Hp = myERA.Hankel_mat2
@@ -115,58 +109,8 @@ class testERA(unittest.TestCase):
                                     col*num_inputs:(col+1)*num_inputs],
                                 C*(A**((row+col)*sample_interval+1))*B)
 
-    def test_set_impulse_outputs(self):
-        """
-        Test setting the impulse outputs w/o loading from files
-        
-        set_impulse_outputs is just a call to _correct_format, don't need a test. 
-        Currently not tested.
-        """
-        pass
     
-
-    """
-    Moved load_impulse_outputs to util, so the test is in testutil.py
-    #@unittest.skip('testing others')    
-    def test_load_impulse_outputs(self):
-        
-        Test loading impulse outputs in [t out1 out2 ...] format.
-        
-        Creates outputs, saves them, loads them from ERA instance, tests the loaded outputs
-        are equal to the originals.
-        
-        num_time_steps = 150
-        for num_states in [4,10]:
-            for num_inputs in [1, 4]:
-                for num_outputs in [1, 2, 4, 5]:
-                    for sample_interval in [1,2,4]:
-                        t0 = 10.
-                        dt = N.random.random()
-                        A,B,C = util.drss(num_states, num_inputs, num_outputs)
-                        time_steps = N.zeros(num_time_steps,dtype=int)
-                        time_steps[::2] = sample_interval*N.arange(num_time_steps/2)
-                        time_steps[1::2] =1 + sample_interval*N.arange(num_time_steps/2)
-                        time_steps, outputs = util.impulse(A,B,C,time_steps=time_steps)
-                        
-                        myERA = era.ERA()
-                        
-                        # Convert to real time from discrete time steps
-                        time_values = t0 + dt*N.array(time_steps)
-                        time_steps_true, outputs_true, dt = myERA._correct_format(time_values, outputs)
-
-                        impulse_file_paths = []
-                        # Save Markov parameters to file
-                        for input_num in range(num_inputs):
-                            impulse_file_paths.append(self.impulse_file_path%input_num)
-                            data_to_save = N.concatenate( \
-                              (time_values.reshape(len(time_values),1), outputs[:,:,input_num]), axis=1)
-                            util.save_mat_text(data_to_save, self.impulse_file_path%input_num)
-                        
-                        myERA.load_impulse_outputs(impulse_file_paths)
-                        N.testing.assert_allclose(myERA.outputs, outputs_true)
-                        N.testing.assert_allclose(myERA.time_steps, time_steps_true)
-    """
-
+    
     #@unittest.skip('testing others')    
     def test_ROM(self):
         """
@@ -187,21 +131,19 @@ class testERA(unittest.TestCase):
                 for sample_interval in [1,2]: #3 or higher makes tests fail
                     myERA = era.ERA()
                     A,B,C = util.drss(num_states_plant, num_inputs, num_outputs)
-                    time_steps = N.zeros(num_time_steps,dtype=int)
-                    time_steps[::2] = sample_interval*N.arange(num_time_steps/2)
-                    time_steps[1::2] =1 + sample_interval*N.arange(num_time_steps/2)
+                    time_steps = era.sample_times(num_time_steps, sample_interval)
                     time_steps, outputs = util.impulse(A, B, C, time_steps=time_steps)
+                    time_steps, outputs, dt = era.make_sampled_format(time_steps, outputs)
                     num_time_steps = time_steps.shape[0]
                     time_steps_dense, outputs_dense = util.impulse(
                         A,B,C,time_steps=N.arange(num_time_steps,dtype=int))
                     
-                    myERA.set_impulse_outputs(time_steps, outputs)
+                    myERA.set_outputs(outputs)
                     
                     A_path_computed = self.test_dir+'A_computed.txt'
                     B_path_computed = self.test_dir+'B_computed.txt'
                     C_path_computed = self.test_dir+'C_computed.txt'
                     
-                    myERA.compute_decomp()
                     myERA.compute_ROM(num_states_model)
                     A = myERA.A
                     B = myERA.B
