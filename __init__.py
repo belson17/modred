2a049bef4a5e19d48284a0d1a5789e5165ecf757
Clancy Rowley <cwrowley@princeton.edu>
2012-04-22 23:17:31 -0400

    Minor changes to setup scripts, to specify version number
    Version number now specified in one place, __init__.py
    Module now defines modred.__version__, consistent with PEP 396

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-#__all__ = ['src']
+__version__ = '0.2'
 
 # Make the classes and functions in src available from top namespace.
 from src import *
--- setup.py
+++ setup.py
@@ -2,17 +2,18 @@
 
 from sys import version
 # Check out unittest2, maybe use older pythons. Not 3 though.
-if version < '2.7' or version > '3':
+if version < '2.6' or version > '3':
 		raise ImportError('modaldecomp requires python version 2.7.x')
 # Soon we will need to require numpy 1.7 for a change Jon will make
 # in POD with new squeeze function.
 from distutils.core import setup
+from __init__ import __version__
 setup(name='modred',
-      version='0.1',
-      author='Brandt Belson and Jonathan Tu',
-      author_email='bbelson@princeton.edu, jhtu@princeton.edu',
-      maintainer='Clancy Rowley',
-      maintainer_email='cwrowley@princeton.edu',
+      version=__version__,
+      author='Brandt Belson, Jonathan Tu, and Clancy Rowley',
+      author_email='bbelson@princeton.edu, jhtu@princeton.edu, cwrowley@princeton.edu',
+      maintainer='Brandt Belson',
+      maintainer_email='bbelson@princeton.edu',
       description='Compute modal decompositions and reduced-order models'+\
       		' easily, efficiently, and in parallel.',
       classifiers='',
