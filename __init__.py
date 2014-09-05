69c980c9202307b3c2ddc0f00e77bf100565450c
Clancy Rowley <cwrowley@princeton.edu>
2014-09-05 10:27:32 -0400

    Rename src/ to modred/
    This way the package can be imported from the source directory
    without first installing with setup.py

--- __init__.py
+++
@@ -1,6 +0,0 @@
-__version__ = '1.0.2'
-# http://en.wikipedia.org/wiki/Software_versioning
-
-# Make the classes and functions in src available from top namespace.
-from src import *
-from examples import *
