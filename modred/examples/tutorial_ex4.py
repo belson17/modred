558d15f2aabc7f7be9a1b60e3aaba41f224a5090
Jonathan Tu (work) <jhtu@users.noreply.github.com>
2017-01-24 10:05:04 -0800

    Examples now write to subdirectories, which are ignored by git.

--- modred/examples/tutorial_ex4.py
+++ modred/examples/tutorial_ex4.py
@@ -1,34 +1,43 @@
 from future.builtins import range
+import os
 
 import numpy as np
 
 import modred as mr
 
 
-num_vecs = 100
+# Create directory for output files
+out_dir = 'tutorial_ex4_out'
+if not os.path.isdir(out_dir):
+    os.makedirs(out_dir)
 
-# uniform grid and corresponding inner product weights.
+# Uniform grid and corresponding inner product weights
+num_vecs = 100
 nx = 80
 ny = 100
 x_grid = 1. - np.cos(np.linspace(0, np.pi, nx))
-y_grid = np.linspace(0, 1., ny)**2
+y_grid = np.linspace(0, 1., ny) ** 2
 Y, X = np.meshgrid(y_grid, x_grid)
 
-snapshots = [mr.VecHandlePickle('vec%d.pkl'%i) for i in range(num_vecs)]
+snapshots = [
+    mr.VecHandlePickle('%s/vec%d.pkl' % (out_dir, i)) for i in range(num_vecs)]
 parallel = mr.parallel_default_instance
 if parallel.is_rank_zero():
     for i,snap in enumerate(snapshots):
         snap.put(np.sin(X * i) + np.cos(Y * i))
-parallel.barrier()
+        parallel.barrier()
 
 weighted_IP = mr.InnerProductTrapz(x_grid, y_grid)
 
-# Calculate DMD modes and save them to pickle files.
+# Calculate DMD modes and save them to pickle files
 my_DMD = mr.DMDHandles(weighted_IP)
 my_DMD.compute_decomp(snapshots)
 my_DMD.put_decomp(
-    'eigvals.txt', 'R_low_order_eigvecs.txt', 'L_low_order_eigvecs.txt',
-    'correlation_mat_eigvals.txt', 'correlation_mat_eigvecs.txt')
+    '%s/eigvals.txt' % out_dir, '%s/R_low_order_eigvecs.txt' % out_dir,
+    '%s/L_low_order_eigvecs.txt' % out_dir,
+    '%s/correlation_mat_eigvals.txt' % out_dir,
+    '%s/correlation_mat_eigvecs.txt' % out_dir)
 mode_indices = [1, 4, 5, 0, 10]
-modes = [mr.VecHandlePickle('mode%d.pkl'%i) for i in mode_indices]
+modes = [
+    mr.VecHandlePickle('%s/mode%d.pkl' % (out_dir, i)) for i in mode_indices]
 my_DMD.compute_exact_modes(mode_indices, modes)
--- modred/examples/tutorial_ex5.py
+++ modred/examples/tutorial_ex5.py
@@ -1,23 +1,28 @@
 from __future__ import print_function
 from future.builtins import range
+import os
 
 import numpy as np
 
 import modred as mr
 
 
-num_elements = 2000
+# Create directory for output files
+out_dir = 'tutorial_ex5_out'
+if not os.path.isdir(out_dir):
+    os.makedirs(out_dir)
+
+# Artificial sample times used as quadrature weights in POD
 num_vecs = 100
-
-# Artificial sample times used as quadrature weights in POD.
 quad_weights = np.logspace(1., 3., num=num_vecs)
-
-base_vec_handle = mr.VecHandlePickle('base_vec.pkl')
-snapshots = [mr.VecHandlePickle(
-    'vec%d.pkl' %i, base_vec_handle=base_vec_handle, scale=quad_weights[i])
+base_vec_handle = mr.VecHandlePickle('%s/base_vec.pkl' % out_dir)
+snapshots = [
+    mr.VecHandlePickle(
+        '%s/vec%d.pkl' % (out_dir, i),base_vec_handle=base_vec_handle,
+        scale=quad_weights[i])
     for i in range(num_vecs)]
 
-# Save arbitrary data, normally unnecessary.
+# Save arbitrary data, normally unnecessary
 num_elements = 2000
 parallel = mr.parallel_default_instance
 if parallel.is_rank_zero():
@@ -25,13 +30,14 @@ if parallel.is_rank_zero():
         snap.put(np.random.random(num_elements))
 parallel.barrier()
 
-# Compute and save POD modes.
+# Compute and save POD modes
 my_POD = mr.PODHandles(np.vdot)
 my_POD.compute_decomp(snapshots)
-my_POD.put_decomp('sing_vals.txt', 'sing_vecs.txt')
-my_POD.put_correlation_mat('correlation_mat.txt')
+my_POD.put_decomp('%s/sing_vals.txt' % out_dir, '%s/sing_vecs.txt' % out_dir)
+my_POD.put_correlation_mat('%s/correlation_mat.txt' % out_dir)
 mode_indices = [1, 4, 5, 0, 10]
-modes = [mr.VecHandleArrayText('mode%d.txt'%i) for i in mode_indices]
+modes = [
+    mr.VecHandleArrayText('%s/mode%d.txt' % (out_dir, i)) for i in mode_indices]
 my_POD.compute_modes(mode_indices, modes)
 
 # Check that modes are orthonormal
