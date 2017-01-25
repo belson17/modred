226acef021886b329dd75be371cf994cd4f7f1f8
Brandt Belson <brandt@mocapanalytics.com>
2017-01-24 19:50:17 -0800

    Changed location of okid test files in test. less strict tol for impulse

--- modred/tests/testokid.py
+++ modred/tests/testokid.py
@@ -33,7 +33,7 @@ def diff(arr_measured, arr_true, normalize=False):
 @unittest.skipIf(parallel.is_distributed(), 'Only test OKID in serial')
 class TestOKID(unittest.TestCase):
     def setUp(self):
-        self.test_dir = join(os.path.dirname(__file__), 'files_okid')
+        self.test_dir = join(os.path.dirname(__file__), '..', 'test_files_okid')
 
 
     def tearDown(self):
