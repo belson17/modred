1980ce01bbf258c5c2afabedc1caf3d59fe87bad
Brandt Belson <belson17@gmail.com>
2017-09-10 22:43:44 -0700

    Print path, debug

--- doc/conf.py
+++ doc/conf.py
@@ -23,7 +23,7 @@ import sys
 sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
 sys.path.insert(0, os.path.join(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'), 'examples'))
 sys.path.insert(0, os.path.join(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'), 'modred'))
-
+print("Path", sys.path)
 
 # -- General configuration ------------------------------------------------
 
