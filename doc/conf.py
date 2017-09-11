6c3fb300cf95fd7d9bcd6d57f4c9781c9ffbbf68
Brandt Belson <belson17@gmail.com>
2017-09-10 22:36:52 -0700

    More path changes for sphinx

--- doc/conf.py
+++ doc/conf.py
@@ -20,9 +20,9 @@
 import os
 import sys
 # sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.join(os.path.abspath(__file__), '..'))
-sys.path.insert(0, os.path.join(os.path.join(os.path.abspath(__file__), '..'), 'examples'))
-sys.path.insert(0, os.path.join(os.path.join(os.path.abspath(__file__), '..'), 'modred'))
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
+sys.path.insert(0, os.path.join(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'), 'examples'))
+sys.path.insert(0, os.path.join(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'), 'modred'))
 
 
 # -- General configuration ------------------------------------------------
