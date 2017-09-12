2e4ad59427d8d116c1f065bdb6ddc169b53bcf5b
Brandt Belson <belson17@gmail.com>
2017-09-11 21:47:23 -0700

    Modified paths, debug rtd

--- doc/conf.py
+++ doc/conf.py
@@ -20,10 +20,12 @@
 import os
 import sys
 # sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
+# sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
 sys.path.insert(0, os.path.join(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'), 'examples'))
+# sys.path.insert(0, os.path.join(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'), 'modred'))
 sys.path.insert(0, os.path.join(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'), 'modred'))
 print("Path", sys.path)
+from modred import bpod
 
 # -- General configuration ------------------------------------------------
 
