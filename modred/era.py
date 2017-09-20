fb36e556d4212729a57d6d4937c1eeb9f929c084
Jonathan Tu (work) <jhtu@users.noreply.github.com>
2017-09-20 14:01:06 -0700

    Fixed bug in ERA tests.
    Some indices were floats not ints.  Now all tests pass in serial and parallel.

--- modred/era.py
+++ modred/era.py
@@ -12,28 +12,28 @@ from . import util
 
 def make_sampled_format(times, Markovs, dt_tol=1e-6):
     """Converts samples at [0 1 2 ...] into samples at [0 1 1 2 2 3 ...].
-        
+
     Args:
         ``times``: Array of time values or time step indices.
-        
+
         ``Markovs``: Array of Markov parameters with indices [time, output,
         input]. ``Markovs[i]`` is the Markov parameter :math:`C A^i B`.
-    
+
     Kwargs:
         ``dt_tol``: Allowable deviation from uniform time steps.
-    
+
     Returns:
         ``time_steps``: Array of time step indices, [0 1 1 2 2 3 ...].
-        
+
         ``Markovs``: Output array at the time step indices.
-        
+
         ``dt``: Time interval between each time step.
-    
+
     Takes a series of data at times :math:`dt*[0 1 2 3 ...]` and duplicates
     entries so that the result is sampled at :math:`dt*[0 1 1 2 2 3 ...]`.
     When the second format is used in the eigensystem realization algorithm
     (ERA), the resulting model has a time step of :math:`dt` rather than
-    :math:`2*dt`. 
+    :math:`2*dt`.
     """
     num_time_steps, num_Markovs, num_inputs = Markovs.shape
     if num_time_steps != times.shape[0]:
@@ -42,7 +42,7 @@ def make_sampled_format(times, Markovs, dt_tol=1e-6):
     dt = times[1] - times[0]
     if np.amax(np.abs(np.diff(times)-dt)) > dt_tol:
         raise ValueError('Data is not equally spaced in time')
-    
+
     time_steps = np.round((times-times[0])/dt)
     num_time_steps_corr = (num_time_steps - 1)*2
     Markovs_corr = np.zeros((num_time_steps_corr, num_Markovs, num_inputs))
@@ -57,31 +57,31 @@ def compute_ERA_model(Markovs, num_states):
     """Convenience function to compute linear time-invariant (LTI)
     reduced-order model (ROM) matrices A, B, and C using the eigensystem
     realization algorithm (ERA) with default settings.
-    
+
     Args:
         ``Markovs``: Array of Markov parameters with indices [time, output,
         input]. ``Markovs[i]`` is the Markov parameter :math:`C A^i B`.
-                
+
         ``num_states``: Number of states in reduced-order model.
-    
+
     Returns:
         ``A``: A matrix of reduced-order model.
 
         ``B``: B matrix of reduced-order model.
 
         ``C``: C matrix of reduced-order model.
-    
+
     Usage::
-     
+
       # Obtain ``Markovs`` array w/indicies [time, output, input]
       num_states = 20
       A, B, C = compute_ERA_model(Markovs, num_states)
-    
+
     Notes:
 
-    - Markov parameters are defined as 
+    - Markov parameters are defined as
       :math:`[CB, CAB, CA^PB, CA^(P+1)B, ...]`
-    
+
     - The functions :py:func:`util.load_signals` and
       :py:func:`util.load_multiple_signals` are often useful.
     """
@@ -92,47 +92,47 @@ def compute_ERA_model(Markovs, num_states):
 class ERA(object):
     """Eigensystem realization algorithm (ERA), implemented for discrete-time
     systems.
-    
+
     Kwargs:
         ``put_mat``: Function to put a matrix out of modred, e.g., write it to
         file.
-    
+
         ``mc``: Number of Markov parameters for controllable dimension of
         Hankel matrix.
-            
+
         ``mo``: Number of Markov parameters for observable dimension of Hankel
         matrix.
-        
+
         ``verbosity``: 1 prints progress and warnings, 0 prints almost nothing.
 
     Computes reduced-order model (ROM) of a discrete-time system, using output
-    data from an impulse response. 
+    data from an impulse response.
 
     Simple usage::
-      
+
       # Obtain array "Markovs" with dims [time, output, input]
       myERA = ERA()
       A, B, C = myERA.compute_model(Markovs, 50)
       sing_vals = myERA.sing_vals
-      
+
     Another example::
-    
+
       # Obtain Markov parameters
       myERA = ERA()
       myERA.compute_model(Markovs, 50)
       myERA.put_model('A.txt', 'B.txt', 'C.txt')
-   
+
     Notes:
 
     - Default values of ``mc`` and ``mo`` are equal and maximal for a balanced
       model.
-        
+
     - The Markov parameters are to be given in the time-sampled format:
       :math:`dt*[0, 1, P, P+1, 2P, 2P+1, ... ]`
-    
-    - The special case where :math:`P=2` results in 
+
+    - The special case where :math:`P=2` results in
       :math:`dt*[0, 1, 2, 3, ... ]`, see :py:func:`make_sampled_format`.
-    
+
     - The functions :py:func:`util.load_signals` and
       :py:func:`util.load_multiple_signals` are often useful.
 
@@ -159,7 +159,7 @@ class ERA(object):
         self.Hankel_mat2 = None
         self.num_Markovs = None
         self.Markovs = None
-    
+
     def compute_model(self, Markovs, num_states, mc=None, mo=None):
         """Computes the A, B, and C matrices of the linear time-invariant (LTI)
         reduced-order model (ROM).
@@ -167,22 +167,22 @@ class ERA(object):
         Args:
             ``Markovs``: Array of Markov parameters with indices [time, output,
             input]. ``Markovs[i]`` is the Markov parameter :math:`C A^i B`.
-                
+
             ``num_states``: Number of states in reduced-order model.
-            
+
         Kwargs:
             ``mc``: Number of Markov parameters for controllable dimension of
             Hankel matrix.
-            
+
             ``mo``: Number of Markov parameters for observable dimension of
             Hankel matrix.
-        
+
         Assembles the Hankel matrices from self.Markovs and computes a singular
         value decomposition. Uses the results to form the A, B, and C matrices.
-        
+
         Note that the default values of ``mc`` and ``mo`` are equal and maximal
-        for a balanced model.            
-        
+        for a balanced model.
+
         Tip: For discrete time systems the impulse is applied over a time
         interval :math:`dt` and so has a time-integral :math:`1*dt` rather than
         :math:`1`.  This means the reduced B matrix is "off" by a factor of
@@ -190,46 +190,46 @@ class ERA(object):
         """
         #SVD is ``L_sing_vecs*np.mat(np.diag(sing_vals))*\
         #    R_sing_vecs.H = Hankel_mat``
-        self._set_Markovs(Markovs)       
+        self._set_Markovs(Markovs)
         self.mc = mc
         self.mo = mo
 
         self._assemble_Hankel()
         self.L_sing_vecs, self.sing_vals, self.R_sing_vecs = \
-            util.svd(self.Hankel_mat) 
+            util.svd(self.Hankel_mat)
 
         # Truncate matrices
         Ur = np.mat(self.L_sing_vecs[:, :num_states])
         Er = np.squeeze(self.sing_vals[:num_states])
         Vr = np.mat(self.R_sing_vecs[:, :num_states])
-        
+
         self.A = np.mat(np.diag(Er**-.5)) * Ur.H * self.Hankel_mat2 * Vr * \
             np.mat(np.diag(Er**-.5))
-        self.B = (np.mat(np.diag(Er**.5)) * (Vr.H)[:, :self.num_inputs]) 
+        self.B = (np.mat(np.diag(Er**.5)) * (Vr.H)[:, :self.num_inputs])
         # *dt above is removed, users must do this themselves.
         # It is explained in the docs.
-        
+
         self.C = Ur[:self.num_Markovs] * np.mat(np.diag(Er**.5))
-        
+
         if (np.abs(np.linalg.eigvals(self.A)) >= 1.).any() and self.verbosity:
             print('Warning: Unstable eigenvalues of reduced A matrix')
             print('eig vals are', np.linalg.eigvals(self.A))
         return self.A, self.B, self.C
- 
+
     def put_model(self, A_dest, B_dest, C_dest):
         """Puts the A, B, and C matrices of the linear time-invariant (LTI)
         reduced-order model (ROM) in destinations (file or memory).
 
-        Args:        
+        Args:
             ``A_dest``: Destination in which to put A matrix of reduced-order
             model.
-            
+
             ``B_dest``: Destination in which to put B matrix of reduced-order
             model.
-            
+
             ``C_dest``: Destination in which to put C matrix of reduced-order
             model.
-        """  
+        """
         self.put_mat(self.A, A_dest)
         self.put_mat(self.B, B_dest)
         self.put_mat(self.C, C_dest)
@@ -238,23 +238,23 @@ class ERA(object):
             print(A_dest)
             print(B_dest)
             print(C_dest)
-      
+
     def put_decomp(
         self, sing_vals_dest, L_sing_vecs_dest, R_sing_vecs_dest,
         Hankel_mat_dest, Hankel_mat2_dest):
         """Puts the decomposition matrices and Hankel matrices in destinations
         (file or memory).
-       
-        Args:        
+
+        Args:
             ``sing_vals_dest``: Destination in which to put Hankel singular
             values.
 
             ``L_sing_vecs_dest``: Destination in which to put left singular
             vectors of Hankel matrix.
-            
+
             ``R_sing_vecs_dest``: Destination in which to put right singular
             vectors of Hankel matrix.
-            
+
             ``Hankel_mat_dest``: Destination in which to put Hankel matrix.
 
             ``Hankel_mat2_dest``: Destination in which to put second Hankel
@@ -265,17 +265,17 @@ class ERA(object):
         self.put_mat(self.L_sing_vecs, L_sing_vecs_dest)
         self.put_mat(self.sing_vals, sing_vals_dest)
         self.put_mat(self.R_sing_vecs, R_sing_vecs_dest)
-        
+
     def put_sing_vals(self, sing_vals_dest):
         """Puts the singular values to ``sing_vals_dest``."""
         self.put_mat(self.sing_vals, sing_vals_dest)
- 
+
     def _set_Markovs(self, Markovs):
         """Sets the Markov params to ``self.Markovs`` and error checks.
-        
+
         Args:
             ``Markovs``: Array of Markov params w/indices [time, output, input].
-        
+
         ``Markovs[i]`` is the Markov parameter C*A**i*B.
         """
         self.Markovs = Markovs
@@ -283,45 +283,45 @@ class ERA(object):
         if ndims == 1:
             self.Markovs = self.Markovs.reshape((self.Markovs.shape[0], 1, 1))
         elif ndims == 2:
-            self.Markovs = self.Markovs.reshape((self.Markovs.shape[0], 
+            self.Markovs = self.Markovs.reshape((self.Markovs.shape[0],
                 self.Markovs.shape[1], 1))
-        elif ndims > 3: 
+        elif ndims > 3:
             raise RuntimeError('Markovs can have 1, 2, or 3 dims, '
                 'yours had %d'%ndims)
-        
+
         self.num_time_steps, self.num_Markovs, self.num_inputs = \
             self.Markovs.shape
-        
+
         # Must have an even number of time steps, remove last entry if odd.
         if self.num_time_steps % 2 != 0:
             self.num_time_steps -= 1
             self.Markovs = self.Markovs[:-1]
- 
+
     def _assemble_Hankel(self):
         """Assembles and sets ``self.Hankel_mat`` and ``self.Hankel_mat2``
-        
-        See variables H and H' in Ma 2011.        
+
+        See variables H and H' in Ma 2011.
         """
-        # To understand the default choice of mc and mo, 
+        # To understand the default choice of mc and mo,
         # consider (let sample_interval=P=1 w.l.o.g.)
         # sequences
         # [0 1 1 2 2 3] => H = [[0 1][1 2]]   H2 = [[1 2][2 3]], mc=mo=1, nt=6
-        # [0 1 1 2 2 3 3 4 4 5] => H=[[0 1 2][1 2 3][2 3 4]] 
+        # [0 1 1 2 2 3 3 4 4 5] => H=[[0 1 2][1 2 3][2 3 4]]
         # and H2=[[1 2 3][2 3 4][3 4 5]], mc=mo=2,nt=10
         # Thus, using all available data means mc=mo=(nt-2)/4.
         # For additional output times, (nt-2)/4 rounds down, so
         # we use this formula.
-        
+
         if self.mo is None or self.mc is None:
             # Set mo and mc, time_steps is always in format [0, 1, P, P+1, ...]
             self.mo = (self.num_time_steps-2)//4
             self.mc = self.mo
-                
+
         if (self.mo + self.mc) +2 > self.num_time_steps:
             raise ValueError('mo+mc+2=%d and must be <= than the number of '
                 'samples %d'%(self.mo+self.mc+2, self.num_time_steps))
-        
-        self.Hankel_mat = np.zeros((self.num_Markovs*self.mo, 
+
+        self.Hankel_mat = np.zeros((self.num_Markovs*self.mo,
             self.num_inputs*self.mc))
         self.Hankel_mat2 = np.zeros(self.Hankel_mat.shape)
         #Markovs_flattened = \
@@ -336,4 +336,3 @@ class ERA(object):
                     self.Markovs[2*(row+col)]
                 self.Hankel_mat2[row_start:row_end, col_start:col_end] = \
                     self.Markovs[2*(row+col)+1]
-                
--- modred/tests/testera.py
+++ modred/tests/testera.py
@@ -17,14 +17,14 @@ from modred import util
 
 def make_time_steps(num_steps, interval):
     """Helper function to find array of integer time steps.
-    
+
     Args:
         num_steps: integer number of time steps to create.
-        
+
         interval: interval between pairs of time steps, as shown above.
-    
+
     Returns:
-        time_steps: array of integers, time steps [0 1 interval interval+1 ...] 
+        time_steps: array of integers, time steps [0 1 interval interval+1 ...]
     """
     if num_steps % 2 != 0:
         raise ValueError('num_steps, %d, must be even'%num_steps)
@@ -44,16 +44,16 @@ class testERA(unittest.TestCase):
         if not os.path.exists(self.test_dir):
             os.mkdir(self.test_dir)
         self.impulse_file_path = join(self.test_dir, 'impulse_input%03d.txt')
-        
+
     def tearDown(self):
         """Deletes all of the matrices created by the tests"""
         rmtree(self.test_dir, ignore_errors=True)
-    
+
     #@unittest.skip('Testing others')
     def test_make_sampled_format(self):
         """
         Test that can give time_values and outputs in either format.
-        
+
         First tests format [0, 1, P, P+1, ...] and if there is a wrong time
         value.
         Then tests [0, 1, 2, 3, ...] format.
@@ -64,10 +64,10 @@ class testERA(unittest.TestCase):
                     sample_interval = 2
                     # P=2 format [0, 1, 2, 3, ...]
                     dt_system = np.random.random()
-                    dt_sample = sample_interval*dt_system                
-                    outputs = np.random.random((num_time_steps, num_outputs, 
+                    dt_sample = sample_interval*dt_system
+                    outputs = np.random.random((num_time_steps, num_outputs,
                         num_inputs))
-                    time_steps = make_time_steps(num_time_steps, 
+                    time_steps = make_time_steps(num_time_steps,
                         sample_interval)
                     time_values = time_steps*dt_system
                     my_ERA = era.ERA()
@@ -76,22 +76,22 @@ class testERA(unittest.TestCase):
                     #self.assertEqual(dt_system_computed, dt_system)
                     num_time_steps_true = (num_time_steps - 1)*2
                     time_steps_true = make_time_steps(num_time_steps_true, 1)
-                    outputs_true = np.zeros((num_time_steps_true, num_outputs, 
+                    outputs_true = np.zeros((num_time_steps_true, num_outputs,
                         num_inputs))
                     outputs_true[::2] = outputs[:-1]
                     outputs_true[1::2] = outputs[1:]
-                    np.testing.assert_allclose(time_steps_computed, 
+                    np.testing.assert_allclose(time_steps_computed,
                         time_steps_true)
                     np.testing.assert_allclose(outputs_computed, outputs_true)
-                    
+
                     # Test that if there is a wrong time value, get an error
-                    time_values[num_time_steps/2] = -1
-                    self.assertRaises(ValueError, era.make_sampled_format, 
+                    time_values[num_time_steps // 2] = -1
+                    self.assertRaises(ValueError, era.make_sampled_format,
                         time_values, outputs)
-                        
+
     #@unittest.skip("testing others")
     def test_assemble_Hankel(self):
-        """ Tests Hankel mats are symmetric given 
+        """ Tests Hankel mats are symmetric given
         ``[CB CAB CA**P CA**(P+1)B ...]``."""
         for num_inputs in [1,3]:
             for num_outputs in [1, 2, 4]:
@@ -99,21 +99,21 @@ class testERA(unittest.TestCase):
                     num_time_steps = 50
                     num_states = 5
                     A,B,C = util.drss(num_states, num_inputs, num_outputs)
-                    time_steps = make_time_steps(num_time_steps, 
+                    time_steps = make_time_steps(num_time_steps,
                         sample_interval)
                     Markovs = util.impulse(A, B, C, time_steps[-1]+1)
                     Markovs = Markovs[time_steps]
-                    
+
                     if sample_interval == 2:
                         time_steps, Markovs = era.make_sampled_format(
                             time_steps, Markovs)
-                    
+
                     myERA = era.ERA(verbosity=0)
                     myERA._set_Markovs(Markovs)
                     myERA._assemble_Hankel()
                     H = myERA.Hankel_mat
                     Hp = myERA.Hankel_mat2
-                    
+
                     for row in range(myERA.mc):
                         for col in range(myERA.mo):
                             np.testing.assert_allclose(
@@ -139,55 +139,55 @@ class testERA(unittest.TestCase):
     def test_compute_model(self):
         """
         Test ROM Markov params similar to those given
-        
+
         - generates data
         - assembles Hankel matrix
         - computes SVD
         - forms the ROM discrete matrices A, B, and C (D=0)
-        - Tests Markov parameters from ROM are approx. equal to full plant's 
+        - Tests Markov parameters from ROM are approx. equal to full plant's
         """
         num_time_steps = 40
         num_states_plant = 12
-        num_states_model = num_states_plant/3
+        num_states_model = num_states_plant // 3
         for num_inputs in [1, 3]:
             for num_outputs in [1, 2]:
-                for sample_interval in [1, 2, 4]: 
-                    time_steps = make_time_steps(num_time_steps, 
+                for sample_interval in [1, 2, 4]:
+                    time_steps = make_time_steps(num_time_steps,
                         sample_interval)
-                    A, B, C = util.drss(num_states_plant, num_inputs, 
+                    A, B, C = util.drss(num_states_plant, num_inputs,
                         num_outputs)
                     myERA = era.ERA(verbosity=0)
                     Markovs = util.impulse(A, B, C, time_steps[-1]+1)
                     Markovs = Markovs[time_steps]
-                    
+
                     if sample_interval == 2:
                         time_steps, Markovs = \
                             era.make_sampled_format(time_steps, Markovs)
                     num_time_steps = time_steps.shape[0]
-                    
+
                     A_path_computed = join(self.test_dir, 'A_computed.txt')
                     B_path_computed = join(self.test_dir, 'B_computed.txt')
                     C_path_computed = join(self.test_dir, 'C_computed.txt')
-                    
+
                     A, B, C = myERA.compute_model(Markovs, num_states_model)
-                    myERA.put_model(A_path_computed, B_path_computed, 
+                    myERA.put_model(A_path_computed, B_path_computed,
                         C_path_computed)
                     #sing_vals = myERA.sing_vals[:num_states_model]
-                    
-                    # Flatten vecs into 2D X and Y mats: 
+
+                    # Flatten vecs into 2D X and Y mats:
                     # [B AB A**PB A**(P+1)B ...]
                     #direct_vecs_flat = np.mat(
                     #    direct_vecs.swapaxes(0,1).reshape(
                     #    (num_states_model,-1)))
-                    
+
                     # Exact grammians from Lyapunov eqn solve
                     #gram_cont = util.solve_Lyapunov(A, B*B.H)
                     #gram_obs = util.solve_Lyapunov(A.H, C.H*C)
                     #print np.sort(np.linalg.eig(gram_cont)[0])[::-1]
                     #print sing_vals
-                    #np.testing.assert_allclose(gram_cont.diagonal(), 
+                    #np.testing.assert_allclose(gram_cont.diagonal(),
                     #    sing_vals, atol=.1, rtol=.1)
-                    #np.testing.assert_allclose(gram_obs.diagonal(), 
+                    #np.testing.assert_allclose(gram_obs.diagonal(),
                     #   sing_vals, atol=.1, rtol=.1)
                     #np.testing.assert_allclose(np.sort(np.linalg.eig(
                     #   gram_cont)[0])[::-1], sing_vals,
@@ -195,13 +195,13 @@ class testERA(unittest.TestCase):
                     #np.testing.assert_allclose(np.sort(np.linalg.eig(
                     #   gram_obs)[0])[::-1], sing_vals,
                     #    atol=.1, rtol=.1)
-                    
+
                     # Check that the diagonals are largest entry on each row
-                    #self.assertTrue((np.max(np.abs(gram_cont),axis=1) == 
+                    #self.assertTrue((np.max(np.abs(gram_cont),axis=1) ==
                     #    np.abs(gram_cont.diagonal())).all())
-                    #self.assertTrue((np.max(np.abs(gram_obs),axis=1) == 
+                    #self.assertTrue((np.max(np.abs(gram_obs),axis=1) ==
                     #    np.abs(gram_obs.diagonal())).all())
-                    
+
                     # Check the ROM Markov params match the full plant's
                     Markovs_model = np.zeros(Markovs.shape)
                     for ti, tv in enumerate(time_steps):
@@ -211,11 +211,11 @@ class testERA(unittest.TestCase):
                     import matplotlib.pyplot as PLT
                     for input_num in range(num_inputs):
                         PLT.figure()
-                        PLT.hold(True)    
+                        PLT.hold(True)
                         for output_num in range(num_outputs):
-                            PLT.plot(time_steps[:50], 
+                            PLT.plot(time_steps[:50],
                             #   Markovs_model[:50, output_num,input_num], 'ko')
-                            PLT.plot(time_steps[:50],Markovs[:50, 
+                            PLT.plot(time_steps[:50],Markovs[:50,
                             #   output_num, input_num],'rx')
                             PLT.plot(time_steps_dense[:50],
                             #   Markovs_dense[:50, output_num, input_num],'b--')
@@ -232,7 +232,6 @@ class testERA(unittest.TestCase):
                     np.testing.assert_allclose(
                         util.load_array_text(C_path_computed), C)
 
-        
+
 if __name__ == '__main__':
     unittest.main()
-        
