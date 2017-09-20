f5a0cfadcd88cced3b5d97892ab3b76434699f08
Jonathan Tu (work) <jhtu@users.noreply.github.com>
2017-09-20 14:25:31 -0700

    Updated version number.

--- modred/_version.py
+++ modred/_version.py
@@ -10,5 +10,10 @@ http://legacy.python.org/dev/peps/pep-0386/
 
 'a' or 'alpha' means alpha version (internal testing),
 'b' or 'beta' means beta version (external testing).
+
+Use -revN (-rev1, -rev2, etc.) to denote post-release revisions having to do
+with packaging alone.  This may be necessary as PyPI no longer allows file
+uploads that share the same name.  As such, any changes to a package requires a
+change in the version number.
 """
-__version__ = '2.0.4'
+__version__ = '2.0.4-rev1'
