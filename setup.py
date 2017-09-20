09acb353bc3fd3a1c8762907944d08b6ed25e169
Jonathan Tu (work) <jhtu@users.noreply.github.com>
2017-09-20 14:46:59 -0700

    Removed personal email addresses.

--- setup.py
+++ setup.py
@@ -34,8 +34,7 @@ setup(
     # keywords='',
     author=('Brandt Belson, Jonathan Tu, and Clancy Rowley;'
             'repacked and ported for Python 3 by Pierre Augier'),
-    author_email=(
-        'brandt.belson@gmail.com, Jonathan.Tu@navy.mil, cwrowley@princeton.edu'),
+    #author_email='',
     url='http://modred.readthedocs.io',
     maintainer=('Brandt Belson, Jonathan Tu, and Clancy Rowley; '
             'repacked & ported for Python3 by Pierre Augier'),
