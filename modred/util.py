4e6f394261008bb220344baba8f5a5ae0774cfd0
Jonathan Tu (work) <jhtu@users.noreply.github.com>
2017-09-26 09:26:42 -0700

    Relaxed tolerance in biorthogonal eigendecomposition.
    User reported failure due to tolerance.  Adjusted absoluted tolerance from
    1e-13 to 1e-12.  This is the tolerance used in many unit tests, so it seems
    sufficiently tight.

--- modred/util.py
+++ modred/util.py
@@ -341,7 +341,7 @@ def eig_biorthog(array, scale_choice='left'):
     L_evals_conj = L_evals.conj()
 
     # Check that evals are the same
-    if not np.allclose(L_evals, R_evals, rtol=1e-10, atol=1e-13):
+    if not np.allclose(L_evals, R_evals, rtol=1e-10, atol=1e-12):
         raise ValueError('Left and right eigenvalues do not match.')
 
     # Sort the evecs
